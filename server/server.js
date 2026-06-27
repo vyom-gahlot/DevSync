a32665e9af136c58edec5dea56970d189ff0cb3f
vyom-gahlot <vyomgahlot07@gmail.com>
2026-06-27 19:15:31 +0530

    imported cors and socket

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
-
+import cors from 'cors';
+import { Socket } from 'socket.io';
 
 
 const app = express();
 
+//Middlewares
 
-app.get('/',(req,res)=>(res.send('API is working')))
+app.use(cors());
+
+app.get('/',(req,res)=>(res.send('API is working')));
 
 const PORT = 3000;
 
 app.listen(PORT, ()=>{
     console.log('server is listening on port' + PORT)
-})
+});
