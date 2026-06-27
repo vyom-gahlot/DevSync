bc890498bf13327d7cd3c6621f68f182f381b924
vyom-gahlot <vyomgahlot07@gmail.com>
2026-06-27 19:10:15 +0530

    created server

--- server/server.js
+++ server/server.js
@@ -3,3 +3,12 @@ import express from 'express';
 
 
 const app = express();
+
+
+app.get('/',(req,res)=>(res.send('API is working')))
+
+const PORT = 3000;
+
+app.listen(PORT, ()=>{
+    console.log('server is listening on port' + PORT)
+})
