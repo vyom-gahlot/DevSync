8d309655cbc1406481c6fcf15f68efa2463b572f
vyom-gahlot <vyomgahlot07@gmail.com>
2026-06-29 22:22:59 +0530

    changed logo, removed the image going text only now

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   return (
     <div className='navbar-full'>
         <div className="navbar-top">
-            <img src={assets.dev_sync_logo} alt="" />
-            <button> Sign In</button>
+            <img src={assets.dev_sync_logo} alt="logo" className='navbar-logo' />
+            <button className='sign-in-button'> Sign In</button>
         </div>
       
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 import './Home.css'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home-div'>
       <Navbar/>
+      <Header/>
     </div>
   )
 }
