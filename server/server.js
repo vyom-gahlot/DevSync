996867756cf5036e32e6518173cc8bba92115db6
vyom-gahlot <vyomgahlot07@gmail.com>
2026-07-02 00:40:39 +0530

    created userSchema

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import { Socket } from 'socket.io';
+import dns from 'dns';
+import connectDB from './configs/db.js';
+import dotenv from 'dotenv';
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+dotenv.config();
 
 
 const app = express();
 
+await connectDB();
+
 //Middlewares
 
 app.use(cors());
+app.use(express.json());
 
+
+//Routes
 app.get('/',(req,res)=>(res.send('API is working')));
 
 const PORT = 3000;
