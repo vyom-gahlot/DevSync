e7cd5208e85ee3b4696b72262df666a12aa7c13a
vyom-gahlot <vyomgahlot07@gmail.com>
2026-07-06 17:54:08 +0530

    fix : missing imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import CreateRoom from './pages/CreateRoom'
-import JoinRoom from './pages/JoinRoom'
-import PageNotFound from './components/PageNotFound'
-import SignIn from './components/SignIn'
-import SignUp from './components/SignUp'
-import { useAppContext } from '../context/AppContext'
+import Home from './pages/Home.jsx'
+import CreateRoom from './pages/CreateRoom.jsx'
+import JoinRoom from './pages/JoinRoom.jsx'
+import PageNotFound from './components/PageNotFound.jsx'
+import SignIn from './components/SignIn.jsx'
+import SignUp from './components/SignUp.jsx'
+import { useAppContext } from '../context/AppContext.jsx'
 import {Toaster} from 'react-hot-toast'
-import Room from './pages/Room'
+import Room from './pages/Room.jsx'
 
 
 
